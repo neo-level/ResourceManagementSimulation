bdadcc327fe7437e65350f2a7415c5b5da1ec99a
neo-level <info@neo-level.com>
2022-01-10 16:48:55 +0100

    Implemented new function to exit the application or playmode respectively, added conditional intructions for the compiler.

--- Assets/Scripts/MenuUIHandler.cs
+++ Assets/Scripts/MenuUIHandler.cs
@@ -1,5 +1,10 @@
+#if UNITY_EDITOR
+using UnityEditor;
+#endif
+
 using Helpers;
 using UnityEngine;
+
 
 // Sets the script to be executed later than all default scripts
 // This is helpful for UI, since other things may need to be initialized before setting the UI
@@ -18,5 +23,17 @@ public class MenuUIHandler : MonoBehaviour
         colorPicker.Init();
         //this will call the NewColorSelected function when the color picker have a color button clicked.
         colorPicker.OnColorChanged += NewColorSelected;
+    }
+
+    /// <summary>
+    /// Closes the application.
+    /// </summary>
+    private void Exit()
+    {
+#if UNITY_EDITOR
+        EditorApplication.ExitPlaymode();
+#else
+        Application.Quit();
+#endif
     }
 }
