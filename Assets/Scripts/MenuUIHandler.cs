26899b45a3d05aa829b4d1aba131722634c4836d
neo-level <info@neo-level.com>
2022-01-10 16:40:12 +0100

    new function to load the Main scene

--- Assets/Scripts/MenuUIHandler.cs
+++ Assets/Scripts/MenuUIHandler.cs
@@ -1,5 +1,6 @@
 using Helpers;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 // Sets the script to be executed later than all default scripts
 // This is helpful for UI, since other things may need to be initialized before setting the UI
@@ -18,5 +19,13 @@ public class MenuUIHandler : MonoBehaviour
         colorPicker.Init();
         //this will call the NewColorSelected function when the color picker have a color button clicked.
         colorPicker.OnColorChanged += NewColorSelected;
+    }
+
+    /// <summary>
+    /// Loads the Main scene.
+    /// </summary>
+    private void StartNew()
+    {
+        SceneManager.LoadScene(1);
     }
 }
